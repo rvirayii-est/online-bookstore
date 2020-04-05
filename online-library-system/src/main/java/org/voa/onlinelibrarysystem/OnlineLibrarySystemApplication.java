ca0a588ff2c72ba94d0dbf8e257800f6a88a4b23
rvirayii <robertovirayii@gmail.com>
2020-04-06 02:53:43 +0800

    secondcommit

---
+++ online-library-system/src/main/java/org/voa/onlinelibrarysystem/OnlineLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package org.voa.onlinelibrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ online-library-system/src/test/java/org/voa/onlinelibrarysystem/OnlineLibrarySystemApplicationTests.java
@@ -0,0 +1,16 @@
+package org.voa.onlinelibrarysystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineLibrarySystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
